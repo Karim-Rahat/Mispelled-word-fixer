3acb48c3b4afe6dcb3e9a1db12ba4b67e8851634
Nurul Karim Rahat <91688197+Karim-Rahat@users.noreply.github.com>
2022-10-04 01:16:09 +0600

    Added validation

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+const mispelledWord = document.getElementById("mispelledWord");
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
 const numberInWords = [
   "Zero",
   "One",
@@ -24,8 +27,15 @@ const numbers = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
-const mispelledWord = document.getElementById("mispelledWord");
-function myFunction() {
+btn.addEventListener("click", function (e) {
+  let flag = false;
+  if (mispelledWord.value.length == 0) {
+    result.innerHTML = "";
+    mispelledWord.setCustomValidity("Type something....");
+    mispelledWord.reportValidity();
+    return false;
+  }
+
   let count = 0;
   numberInWords.map((data, i) => {
     if (data.length == mispelledWord.value.length) {
@@ -36,10 +46,14 @@ function myFunction() {
         if (item == mispelledWordSplit[j]) {
           count++;
           if (mispelledWord.value.length - 1 == count) {
-            result.innerHTML = `Let me guess : ` + data + `(${numbers[i]})`;
+            result.innerHTML = `Let me guess : ` + data.toLowerCase() + ` (${numbers[i]})`;
+            flag = true;
           }
         }
       });
     }
+    if (flag == false) {
+      result.innerHTML = "No matched found";
+    }
   });
-}
+});
