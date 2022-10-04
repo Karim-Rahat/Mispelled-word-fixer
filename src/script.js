2e9393923ed38056955286973d7e2c86eed182aa
Nurul Karim Rahat <91688197+Karim-Rahat@users.noreply.github.com>
2022-10-05 01:35:50 +0600

    added data upto forty

--- src/script.js
+++ src/script.js
@@ -22,12 +22,36 @@ const numberInWords = [
   "Seventeen",
   "Eighteen",
   "Nineteen",
+  "TWENTY",
+  "Twenty-one",
+  "Twenty-two",
+  "Twenty-three",
+  "Twenty-four",
+  "Twenty-five",
+  "Twenty-six",
+  "Twenty-seven",
+  "Twenty-eight",
+  "Twenty-nine",
+  "Thirty",
+  "Thirty-one",
+  "Thirty-two",
+  "Thirty-three",
+  "Thirty-four",
+  "Thirty-five",
+  "Thirty-six",
+  "Thirty-seven",
+  "Thirty-eight",
+  "Thirty-nine",
+  "Forty",
 ];
 const numbers = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
 ];
 
 btn.addEventListener("click", function (e) {
+mispelledWord.value=mispelledWord.value.replace(" ","-")
+ 
   let flag = false;
   if (mispelledWord.value.length == 0) {
     result.innerHTML = "";
@@ -46,7 +70,8 @@ btn.addEventListener("click", function (e) {
         if (item == mispelledWordSplit[j]) {
           count++;
           if (mispelledWord.value.length - 1 == count) {
-            result.innerHTML = `Let me guess : ` + data.toLowerCase() + ` ( ${numbers[i]})`;
+            result.innerHTML =
+              `Let me guess : ` + data.toLowerCase() + ` ( ${numbers[i]})`;
             flag = true;
           }
         }
